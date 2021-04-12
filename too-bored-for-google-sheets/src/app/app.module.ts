5017ccc76ca2ceac742790277b3d4c15f6611bd8
batman-that-laughs <b4tm4n.th4t.l4ughs@gmail.com>
2021-04-12 09:39:14 +0000

    single entry ui

--- too-bored-for-google-sheets/src/app/app.module.ts
+++ too-bored-for-google-sheets/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEntryComponent } from './add-entry/add-entry.component';
+
+import { ReactiveFormsModule } from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEntryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
